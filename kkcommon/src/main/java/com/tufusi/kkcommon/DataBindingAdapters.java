1f7808c60da3f1570c33f040500bd75f230ecaca
LeoCheung0221 <leocheung4ever@gmail.com>
2020-08-11 23:51:42 +0800

    init kkwebview project

---
+++ kkcommon/src/main/java/com/tufusi/kkcommon/DataBindingAdapters.java
@@ -0,0 +1,11 @@
+package com.tufusi.kkcommon;
+
+/**
+ * Created by 鼠夏目 on 2020/8/11.
+ *
+ * @author 鼠夏目
+ * @description
+ * @see
+ */
+public class DataBindingAdapters {
+}
---
+++ kkcommon/src/main/java/com/tufusi/kkcommon/autoservice/IWebViewService.java
@@ -0,0 +1,11 @@
+package com.tufusi.kkcommon.autoservice;
+
+/**
+ * Created by 鼠夏目 on 2020/8/11.
+ *
+ * @author 鼠夏目
+ * @description
+ * @see
+ */
+public interface IWebViewService {
+}
